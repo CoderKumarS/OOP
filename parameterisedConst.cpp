1431443d95c28eaa3b143cbda385002fd8b4f379
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2023-09-08 14:40:31 +0530

    parameterised constructor

---
+++ parameterisedConst.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class Rectangle
+{
+private:
+    int length, width;
+
+public:
+    Rectangle(int a, int b)
+    {
+        length = a;
+        width = b;
+    }
+    int area()
+    {
+        int area = (length * width);
+        cout << "Area is : " << area;
+    }
+};
+
+int main()
+{
+    Rectangle r = Rectangle(6,6);
+    r.area();
+    return 0;
+}
